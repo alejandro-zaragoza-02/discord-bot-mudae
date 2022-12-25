8798e424a645ad81d39f8e7edd2846b4e6214c36
alejandro-zaragoza-02 <102815776+alejandro-zaragoza-02@users.noreply.github.com>
2022-12-26 00:01:11 +0100

    Suscriptores

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 require('dotenv/config');
 
+const KAKERA_AVISO = 200;
 var lastMessage = "";
-const commands = ["$w", "$m", "/mx", "$h", "$mx", "$im ucy"];
+const commands = ["$w", "$m", "$h", "$mx", "$im ucy"];
+let suscriptores = new Map();
+suscriptores.set("247330571687034881", "One Piece");
 
 const client = new Client({
     intents: [
@@ -17,19 +20,27 @@ client.on('ready', () => {
 })
 
 client.on('messageCreate', (message) => {
-    if(message.channel.name == 'wifus-todo'){
-        try {
-            if(message.embeds[0].data.description.substring(0, 9) == "One Piece" && commands.includes(lastMessage)){
-                console.log("Avisando...");
-                message.channel.send("<@247330571687034881> Ha salido un personaje de One Piece")
-            }else{
-                console.log("No se esta tirando.");
+    if(message.channel.name == 'wifus-todo' && commands.includes(lastMessage)){
+        suscriptores.forEach((collecion, usuario) => {
+            // Comprobar si la carta es cara
+            try {
+                let kakeras = message.embeds[0].data.description.split('**')[1];
+                if(kakeras > KAKERA_AVISO){
+                    message.channel.send("<@247330571687034881> Ha salido una carta cara");
+                }
+            } catch (error) {
+                console.log("Error al leer los kakeras de la carta");
             }
-        } catch (error) {
-            console.log("No se ha encontrado descripcion");
-        }finally{
-            lastMessage = message.content.toLowerCase();
-        }
+            // Comprobar si la carta pertenece a la coleccion
+            try {
+                if(message.embeds[0].data.description.includes(collecion)){
+                    message.channel.send(`<@${usuario}> Ha salido un personaje de ${collecion}`);
+                }
+            } catch (error) {
+                console.log("No se ha encontrado descripcion");
+            }
+        })
+        lastMessage = message.content.toLowerCase();
     }
 })
 
